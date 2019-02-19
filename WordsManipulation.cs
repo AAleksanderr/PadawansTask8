dd667d26630f43f542422ee90e1ee1cd97ef2d4c
AAleksanderr <indra1@tut.by>
2019-02-20 02:21:25 +0300

    Done1.0

--- WordsManipulation.cs
+++ WordsManipulation.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using System;
+﻿using System;
 
 namespace PadawansTask8
 {
@@ -7,8 +6,18 @@ namespace PadawansTask8
     {
         public static void RemoveDuplicateWords(ref string text)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (text == null) throw new ArgumentNullException();
+            var values = text.Split(new[] {'.', ',', '!', '?', '-', ':', ';', ' '},
+                StringSplitOptions.RemoveEmptyEntries);
+            var marks = text.Split(values, StringSplitOptions.None);
+            for (var i = 0; i <= values.Length - 2; i++)
+            for (var j = i + 1; j < values.Length; j++)
+                if (values[i] == values[j])
+                    values[j] = "";
+            var str = "";
+            for (var i = 0; i < values.Length; i++) str += marks[i] + values[i];
+            str += marks[marks.Length - 1];
+            text = str;
         }
     }
 }
