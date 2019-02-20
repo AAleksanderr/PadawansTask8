b3c1818cc37ca6c09d99c41d65e41f6b0a16dc83
AAleksanderr <indra1@tut.by>
2019-02-20 12:31:16 +0300

    v2.0

--- WordsManipulation.cs
+++ WordsManipulation.cs
@@ -7,17 +7,34 @@ namespace PadawansTask8
         public static void RemoveDuplicateWords(ref string text)
         {
             if (text == null) throw new ArgumentNullException();
+            if (text == "") throw new ArgumentException();
+
+            //Create array words
             var values = text.Split(new[] {'.', ',', '!', '?', '-', ':', ';', ' '},
                 StringSplitOptions.RemoveEmptyEntries);
-            var marks = text.Split(values, StringSplitOptions.None);
+
+            //Create array strings between words
+            var marks = new string[values.Length + 1];
+            var pos = 0;
+            for (var i = 0; i < values.Length; i++)
+            {
+                var pos2 = text.IndexOf(values[i], pos, StringComparison.Ordinal);
+                marks[i] = text.Substring(pos, pos2 - pos);
+                pos = pos2 + values[i].Length;
+            }
+
+            marks[values.Length] = text.Substring(pos, text.Length - pos);
+
+            //Delete duplicate
             for (var i = 0; i <= values.Length - 2; i++)
             for (var j = i + 1; j < values.Length; j++)
                 if (values[i] == values[j])
                     values[j] = "";
-            var str = "";
-            for (var i = 0; i < values.Length; i++) str += marks[i] + values[i];
-            str += marks[marks.Length - 1];
-            text = str;
+
+            //Collect a new line
+            text = "";
+            for (var i = 0; i < values.Length; i++) text += marks[i] + values[i];
+            text += marks[marks.Length - 1];
         }
     }
 }
